0186a7cb936e40f28127fe4b8e5a7cda1e86beea
OlgaMarhulevich <omarhulevich@yandex.by>
2021-10-17 13:51:01 +0300

    add JSX

--- src/pages/registration/Registration.tsx
+++ src/pages/registration/Registration.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import s from './Registration.module.scss'
+import {InputText} from "../../common/components/inputText/InputText";
+import {Button} from "../../common/components/button/Button";
 
 export const Registration: React.FC = () => {
     return (
         <div className={s.page}>
-            <div className={s.pageTitle}>Registration page</div>
+            <div className={s.pageTitle}>Sign up</div>
+
+            <InputText label={'Email'} error={''} className={s.input}/>
+            <InputText label={'Password'} type={"password"} error={''} className={s.input}/>
+            <InputText label={'Confirm password'} type={"password"} error={''} className={s.input}/>
+            <div>
+                <Button red value={'Cancel'} className={s.btn}/>
+                <Button value={'Register'} className={s.btn}/>
+            </div>
         </div>
     )
 }
